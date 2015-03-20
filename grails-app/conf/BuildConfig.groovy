5fdb8fcfdb091de74d70e9de0b0a0b46c21a831d
manufarfaro <manufarfaro@gmail.com>
2015-03-20 09:27:16 -0300

    jetty downgrade para buildpack

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -58,7 +58,7 @@ grails.project.dependency.resolution = {
     plugins {
         // plugins for the build system only
         //build ":tomcat:7.0.55"
-		build ":jetty:2.0.4"
+		build ":jetty:2.0.3"
 
         // plugins for the compile step
         compile ":scaffolding:2.1.2"
